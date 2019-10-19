ef2a1419722247972d3c849f5946a5d4577ea472
LearnerZ <Z709394@hotmail.com>
2019-10-19 20:54:14 +0800

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -1,1 +1,25 @@
 
+使用 window.alert() 写入警告框
+使用 document.write() 写入 HTML 输出
+使用 innerHTML 写入 HTML 元素
+使用 console.log() 写入浏览器控制台
+
+
+
+<!------------------>
+<script>
+			function a(){
+				document.getElementById('a').innerHTML="123"
+			}
+		</script>
+
+
+
+<p id="a" style="font-size:41px">改变这个的大小</p>
+			
+			<button onclick="a()">1</button>
+
+
+在<head></head>定义js 通过fuction 定义 在body赋值
+
+<!------------------>
