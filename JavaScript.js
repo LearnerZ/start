7415db4053888f26ec66193dbc05c3e0ac28ef01
LearnerZ <Z709394@hotmail.com>
2019-10-27 21:07:16 +0800

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -38,3 +38,26 @@ var x = 7;   // 会执行
 
 
 <!------------------>
+数组的转化
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+document.write(fruits.toString())  //将数组以字符的形式
+
+添加或删除
+fruits.pop() // 将最后一个删除（定义ID就是提取出来）
+
+fruits.push('xxx') //添加xxx到数列（定义ID就是添加并查看数列长度）
+
+fruits.shift() //删除第一个
+
+fruits.unshift('xxx') //添加xxx到第一个
+
+fruits.splice(0,1) //删除特定位置（第一参数是从哪个开始删除，第二参数是删除多少个）
+
+fruits.concat(abc) //fruits跟abc数组合并（也可以数组跟值合并【'1','2'】）
+
+分隔符
+
+document.write(fruits.joins(‘-’)) //用-将数组分开
+
+					
+<!------------------>	
